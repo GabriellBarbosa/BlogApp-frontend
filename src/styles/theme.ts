0e2920c7f98df32fa80cfb344571f03312f78a69
Gabriel Barbosa <bielbarbosa900@gmail.com>
2021-11-13 18:27:16 -0300

    add red color

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -5,7 +5,8 @@ const theme = {
     lightText: '#A9A9A9',
     primary: '#8257e6',
     contentBg: '#0d1117',
-    borderColor: '#30363d'
+    borderColor: '#30363d',
+    error: '#D33B31'
   }
 }
 
