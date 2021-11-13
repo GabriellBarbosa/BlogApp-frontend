b043e2c220432ec80c3a1cfb988b8dd69c9de4a6
Gabriel Barbosa <bielbarbosa900@gmail.com>
2021-11-13 18:26:09 -0300

    add create account functionality

--- src/pages/Register/index.tsx
+++ src/pages/Register/index.tsx
@@ -1,26 +1,112 @@
-import React from 'react'
-import { Container } from './styles'
+import React, { useRef, useState } from 'react'
+import { Container, Title, FormStyled, FieldsWrapper } from './styles'
 import { Input } from '@components/Input'
-import { Form } from '@unform/web'
+import { FormHandles } from '@unform/core'
+import { Button } from '@components/Button'
+import { ResponseError } from '@interfaces/responseError'
+import { api } from '@services/api'
+import * as Yup from 'yup'
+
+interface ErrProps {
+  [key: string]: string
+}
+
+interface FormProps {
+  userName: string
+  email: string
+  password: string
+  confirmPassword: string
+}
+
+interface MyError {
+  response: { data: { message: ResponseError[] } }
+  status: number
+}
 
 export const Register: React.FC = () => {
-  const handleSubmit = (data: any) => {
-    console.log(data)
+  const formRef = useRef<FormHandles>(null)
+  const [loading, setLoading] = useState<boolean>(false)
+
+  const handleSubmit = async (data: FormProps) => {
+    try {
+      setLoading(true)
+      const schema = Yup.object().shape({
+        userName: Yup.string()
+          .min(2, 'Nome muito curto')
+          .required('Campo obrigatório'),
+        email: Yup.string()
+          .email('E-mail inválido')
+          .required('Campo obrigatório'),
+        password: Yup.string()
+          .min(2, 'Senha muito curta')
+          .required('Campo obrigatório'),
+        confirmPassword: Yup.string()
+          .min(2, 'Senha muito curta')
+          .required('Campo obrigatório')
+      })
+
+      formRef.current?.setErrors({})
+
+      await schema.validate(data, {
+        abortEarly: false
+      })
+
+      if (data.confirmPassword != data.password) {
+        formRef.current?.setErrors({
+          password: 'As senhas precisam ser iguais',
+          confirmPassword: 'As senhas precisam ser iguais'
+        })
+        setLoading(false)
+        return
+      }
+
+      const response = await api.post('auth/register', data)
+      window.localStorage.setItem('token', response.data.token)
+
+      setLoading(false)
+    } catch (err: unknown) {
+      setLoading(false)
+      if (err instanceof Yup.ValidationError) {
+        const errorMessages: ErrProps = {}
+        err.inner.forEach(error => {
+          const { path, message } = error
+          if (path && message) {
+            errorMessages[path] = message
+          }
+        })
+        formRef.current?.setErrors(errorMessages)
+        return
+      }
+
+      const { response, status } = err as MyError
+      if (status === 500) {
+        return
+      }
+      const errorMessages: ErrProps = {}
+      response.data.message.forEach((error: ResponseError) => {
+        const { field, message } = error
+        errorMessages[field] = message
+        formRef.current?.setErrors(errorMessages)
+      })
+    }
   }
 
   return (
     <Container>
-      <Form onSubmit={handleSubmit}>
-        <Input label="Nome de usuário" name="userName" type="text" />
-        <Input label="E-mail" name="email" type="email" />
-        <Input label="Senha" name="password" type="password" />
-        <Input
-          label="Confirme a senha"
-          name="confirmPassword"
-          type="password"
-        />
-        <button>Pronto</button>
-      </Form>
+      <FormStyled ref={formRef} onSubmit={handleSubmit}>
+        <Title>Criar conta</Title>
+        <FieldsWrapper>
+          <Input label="Nome de usuário" name="userName" type="text" />
+          <Input label="E-mail" name="email" type="text" />
+          <Input label="Senha" name="password" type="password" />
+          <Input
+            label="Confirme a senha"
+            name="confirmPassword"
+            type="password"
+          />
+        </FieldsWrapper>
+        <Button loading={loading}>Pronto</Button>
+      </FormStyled>
     </Container>
   )
 }
--- src/pages/Register/styles.ts
+++ src/pages/Register/styles.ts
@@ -1,4 +1,5 @@
 import styled from 'styled-components'
+import { Form } from '@unform/web'
 
 export const Container = styled.div`
   width: 100vw;
@@ -7,4 +8,25 @@ export const Container = styled.div`
   justify-content: center;
   align-items: center;
   flex-direction: column;
+`
+
+export const Title = styled.h1`
+  font-size: 28px;
+  margin-bottom: 20px;
+`
+
+export const FormStyled = styled(Form)`
+  padding: 40px;
+  width: 380px;
+  background-color: #161b22;
+  border-radius: 8px;
+  display: flex;
+  flex-direction: column;
+  gap: 10px;
+`
+
+export const FieldsWrapper = styled.div`
+  > div {
+    margin-bottom: 10px;
+  }
 `
